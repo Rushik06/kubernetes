1e3071e81ecad6ee8175d7348187595f2584cfcd
Dumpala Rushik <dumpala.rushik@mindfiresolutions.com>
2026-04-01 21:08:57 +0530

    refactor: refactor server file of service-b

--- services/service-b/src/index.ts
+++ services/service-b/src/index.ts
@@ -1,13 +1,29 @@
 import { logger } from '@shared/utils';
-import { worker } from './worker/worker';
+import { worker } from './worker/worker.js';
 import dotenv from 'dotenv';
+import express from 'express';
+import { register } from './metrics/metrics.js';
 
 // Load env variables
 dotenv.config();
 
 logger.info('Service B worker started');
 
-//Debug event from the worker
+// Metrics server setup
+const app = express();
+
+app.get('/metrics', async (_req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+
+const PORT = process.env.METRICS_PORT || 3001;
+
+app.listen(PORT, () => {
+  logger.info(` Metrics running on port ${PORT}`);
+});
+
+// Debug events from the worker
 worker.on('completed', (job) => {
   logger.info(`job ${job.id} completed sucessfully`);
 });
